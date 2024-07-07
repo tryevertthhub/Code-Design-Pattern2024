0aabe6008509b8c9f0789a32a4516821a072de54
tryevertthhub <tryevertth@gmail.com>
2024-07-08 08:11:12 +0900

    feat: add strategy.cpp in Cpp folder

---
+++ C++/Strategy.cpp
@@ -0,0 +1,70 @@
+class Strategy {
+    public:
+       virtual ~Strategy() = default;
+       virtual std::string doAlgorithm(std::string_view data) const = 0;
+
+};
+
+class Context {
+    private:
+    std::unique_ptr<Strategy> strategy_;
+
+    public:
+    explicit Context(std::unique_ptr<Strategy> &&strategy = {}): strategy_(std::move(strategy))
+    {
+
+    }
+
+    void set_strategy(std::unique_ptr<Strategy> &&strategy)
+    {
+        strategy_ = std::move(strategy);
+    }
+    void doSomeBusinessLogic() const
+    {
+        if (strategy_) {
+            std::cout << "Context: Sorting data using the strategy (not sure how it'll do it)\n";
+            std::string result = strategy_->doAlgorithm("aecbd");
+            std::cout << result << "\n";
+        } else {
+            std::cout << "Context: Strategy isn't set\n";
+        }
+    }
+};
+
+class ConcreteStrategyA : public Strategy
+{
+public:
+    std::string doAlgorithm(std::string_view data) const override
+    {
+        std::string result(data);
+        std::sort(std::begin(result), std::end(result));
+
+        return result;
+    }
+};
+class ConcreteStrategyB : public Strategy
+{
+    std::string doAlgorithm(std::string_view data) const override
+    {
+        std::string result(data);
+        std::sort(std::begin(result), std::end(result), std::greater<>());
+
+        return result;
+    }
+};
+
+void clientCode() 
+{
+    Context context(std::make_unique<ConcreteStrategyA>());
+    std::cout << "Client: Strategy is set to normal sorting.\n";
+    context.doSomeBusinessLogic();
+    std::cout << "\n";
+    context.set_strategy(std::make_unique<ConcreteStrategyB>());
+    context.doSomeBusinessLogic();
+}
+
+int main()
+{
+    clientCode();
+    return 0;
+}
